ee6256c693d8aa1f4fe6d59ddb5367aab103e27c
Dushyant <prashunthakur0804@gmail.com>
2024-11-17 12:05:36 +0530

    added server.js file code

--- server.js
+++ server.js
@@ -1,2 +1,7 @@
-const data = "hello"
-console.log(data)
+const express =require("express");
+const app = express()
+const port = 8080;
+
+app.listen(port,()=>{
+    console.log('server is runninng on the port',port)
+})
